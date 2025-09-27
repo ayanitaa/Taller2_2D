3ad48486cb5e2e88b68c4e026ee2eddee66c21c4
ayanitaa <ayanasalazar7@gmail.com>
2025-09-27 11:23:54 -0500

    Escena Menú
    1. Añadí todas las escenas al Build Profiles.
    
    2. Se crearon todas las escenas correspondientes.
    
    3. Se creó el script y el objeto vacío que contiene a LoaderScenes (En el inspector se asignaron cada uno de los botones). Consta de 3 métodos cada botón lo contiene en su OnClick.
    
    4. Se creó el nombre del juego y se importó el font

---
+++ Taller2/Assets/Scripts/LoaderScenes.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoaderScenes : MonoBehaviour
+{
+    [Header("Menú UI")]
+    public GameObject menuPanel;
+    public Button ButtonIniciar;
+    public Button ButtonInstrucciones;
+    public Button ButtonSalir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IniciarJuego()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene1");
+    }
+
+    public void MostrarInstrucciones()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("InstructionsScene");
+    }
+
+    public void SalirJuego()
+    {
+        Application.Quit();
+    }
+}
